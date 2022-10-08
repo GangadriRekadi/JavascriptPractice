74e403b28b331c28d3eb171300d79929445a2251
GangadriRekadi <rekadigangadri@gmail.com>
2022-10-08 14:39:22 +0530

    Change

--- Calculator/cal.js
+++ Calculator/cal.js
@@ -11,15 +11,14 @@ const operation = document.querySelector("#sign1")
 var firstValue = "";
 var isFirstValue = false;
 
-var secondValue ="";
+var secondValue = "";
 
 var sign = "";
 var resultValue = 0;
 var decimalCount = 0;
 
 var sign1 = "No Operation occuring"
-// var count1 = 0;
-// var count2 = 0;
+
 
 operation.innerHTML = sign1;
 
@@ -46,7 +45,7 @@ signEventListenersCreation();
 
 
 
-clear.addEventListener("click",()=>{
+clear.addEventListener("click", () => {
 
     result.innerHTML = 0;
     firstValue = '';
@@ -57,51 +56,50 @@ clear.addEventListener("click",()=>{
     operation.innerHTML = ` memory cleared`
 })
 
-negative.addEventListener("click",()=>{
+negative.addEventListener("click", () => {
 
-    if(firstValue && (!sign)){
-        // firstValue = parseFloat(firstValue);
+    if (firstValue && (!sign)) {
+
         firstValue *= -1;
         result.innerHTML = firstValue;
         operation.innerHTML = `  sign Changed`
     }
-    else if(secondValue && sign){
-        // secondValue = parseFloat(secondValue);
+    else if (secondValue && sign) {
+
         secondValue *= -1;
         result.innerHTML = secondValue;
         operation.innerHTML = `  sign Changed`
     }
     else if (!firstValue)
-    result.innerHTML = 0
+        result.innerHTML = 0
     else
-    operation.innerHTML = ` ${sign} operation is in middle. Second value should be enter! `;
-   
+        operation.innerHTML = ` ${sign} operation is in middle. Second value should be enter! `;
+
 })
 
-function signEventListenersCreation(){
+function signEventListenersCreation() {
 
-    for(let i = 0; i < signs.length; i++){
+    for (let i = 0; i < signs.length; i++) {
 
-        signs[i].addEventListener('click', 
-        (e)=>{
-           console.log("hello sign");
+        signs[i].addEventListener('click',
+            (e) => {
+                console.log("hello sign");
 
-            calculation();
-            
-            sign = e.target.getAttribute('value'); 
-            sign1 = sign;
-            if(!firstValue){
-            result.innerHTML = 0;
-            isFirstValue = false;
+                calculation();
+
+                sign = e.target.getAttribute('value');
+                sign1 = sign;
+                if (!firstValue) {
+                    result.innerHTML = 0;
+                    isFirstValue = false;
+                }
+                else {
+                    operation.innerHTML = `  ${sign} operation occuring`;
+                    isFirstValue = true;
+                    decimalCount = 0
+                };
+
             }
-            else
-            {
-                operation.innerHTML = `  ${sign} operation occuring`;
-            isFirstValue = true;
-            decimalCount = 0
-            };
-            
-        }
         )
     }
 }
@@ -109,62 +107,59 @@ function signEventListenersCreation(){
 
 
 
-function numberEventListenersCreation(){
+function numberEventListenersCreation() {
 
     // creating event listeners for all number buttons using for loop
 
-for(let i = 0; i < numbers.length; i++){
+    for (let i = 0; i < numbers.length; i++) {
 
-    numbers[i].addEventListener('click', 
-    (eS) => {
-    
-        let value = eS.target.getAttribute('value');
-      
+        numbers[i].addEventListener('click',
+            (eS) => {
 
-    if(!isFirstValue){
-        getFirstValue(value);
+                let value = eS.target.getAttribute('value');
+
+
+                if (!isFirstValue) {
+                    getFirstValue(value);
+                }
+
+                else {
+                    getSecondValue(value);
+                }
+            })
+
     }
 
-    else{
-        getSecondValue(value);
-    }
-    } )
-    
 }
 
-}
+function getFirstValue(value) {
 
-function getFirstValue(value){ 
-    
-    if(value === '.' && (!sign) ){
-            ++decimalCount;
-            // if(sign === "=")
-            // firstValue = "";
-        if(decimalCount >1){
-            
-             return;
+    if (value === '.' && (!sign)) {
+        ++decimalCount;
+
+        if (decimalCount > 1) {
+
+            return;
         }
     }
-   
+
     firstValue += value;
     result.innerHTML = firstValue
 
 
 }
 
-function getSecondValue(value){
+function getSecondValue(value) {
 
-    // result.innerHTML = ""
+    if (sign && firstValue) {
 
-    if(sign && firstValue){
-
-        if(value === '.'){
+        if (value === '.') {
             ++decimalCount;
-        if(decimalCount >1){
-             return;
+            if (decimalCount > 1) {
+                return;
+            }
         }
-    }
-       secondValue += value;
+        secondValue += value;
 
         result.innerHTML = secondValue;
 
@@ -172,73 +167,66 @@ function getSecondValue(value){
 }
 
 
-equals.addEventListener('click',calculation);
+equals.addEventListener('click', calculation);
 
- percent.addEventListener("click",
- ()=>{
-    console.log("percent Event", sign);
+percent.addEventListener("click",
+    () => {
 
-    
-   
-    if(firstValue && (!sign)){
-        console.log("percenr value");
-    firstValue =  firstValue / 100 ;
-    result.innerHTML = firstValue.toFixed(3);
-    operation.innerHTML = "    % Operation Occured";
-}
-    else if(secondValue && sign){
-       
-        secondValue = secondValue / 100;
-        result.innerHTML = secondValue.toFixed(3);
-        operation.innerHTML = "    % Operation Occured";
+        if (firstValue && (!sign)) {
+            firstValue = firstValue / 100;
+            result.innerHTML = firstValue.toFixed(3);
+            operation.innerHTML = "    % Operation Occured";
+        }
+        else if (secondValue && sign) {
+
+            secondValue = secondValue / 100;
+            result.innerHTML = secondValue.toFixed(3);
+            operation.innerHTML = "    % Operation Occured";
+        }
+        else if (!firstValue)
+            result.innerHTML = 0;
+        else
+            operation.innerHTML = ` ${sign} operation is in middle. Second value should be enter! `;
     }
-    else if(!firstValue)
-    result.innerHTML = 0;
-    else
-    operation.innerHTML = ` ${sign} operation is in middle. Second value should be enter! `;
- }
-  
- );
 
-function calculation(){
+);
+
+function calculation() {
 
 
-   
 
-    if(firstValue && secondValue && sign){
+
+    if (firstValue && secondValue && sign) {
 
         firstValue = parseFloat(firstValue);
-         secondValue = parseFloat(secondValue);
+        secondValue = parseFloat(secondValue);
 
-        if(sign === "+")
-        resultValue = firstValue + secondValue;
+        if (sign === "+")
+            resultValue = firstValue + secondValue;
         else if (sign === "-")
-        resultValue = firstValue - secondValue;
-        else if(sign === "x")
-        resultValue = firstValue * secondValue;
-        else if(sign === "/")
-        resultValue = firstValue/secondValue;
+            resultValue = firstValue - secondValue;
+        else if (sign === "x")
+            resultValue = firstValue * secondValue;
+        else if (sign === "/")
+            resultValue = firstValue / secondValue;
 
-        firstValue = resultValue.toFixed(3); 
-    result.innerHTML = firstValue; 
+        firstValue = resultValue.toFixed(3);
+        result.innerHTML = firstValue;
     }
 
-    else if(!firstValue)
-    result.innerHTML = 0;
-    else
-    {
+    else if (!firstValue)
+        result.innerHTML = 0;
+    else {
         firstValue = parseFloat(firstValue);
         result.innerHTML = firstValue.toFixed(3);
-        operation.innerHTML = "    Final answer is..... "
+
     }
 
-    
-    
-    
-   
+    operation.innerHTML = "    Final answer is..... "
+
     secondValue = "";
     decimalCount = 0;
     sign = ""
-    if(firstValue)
-    isFirstValue = true;
+    if (firstValue)
+        isFirstValue = true;
 }
--- Calculator/themeChange.js
+++ Calculator/themeChange.js
@@ -3,22 +3,22 @@
 const btn = document.querySelector('.theme-btn')
 
 
-function random(number){
+function random(number) {
     return Math.floor(Math.random() * (number + 1));
 }
 
- var clicks = 0;
+var clicks = 0;
 
 btn.addEventListener('click', () => {
 
- 
-    console.log(++clicks )
+
+    console.log(++clicks)
     console.log(Object(), Object.prototype);
     console.log("hii");
     console.log(Object.prototype.constructor.prototype.constructor.prototype);
 
-    const rndcol = `rgb(${random(255)}, ${random(255)}, ${random(225)})` ;
-    document.body.style.backgroundColor =  rndcol;
+    const rndcol = `rgb(${random(255)}, ${random(255)}, ${random(225)})`;
+    document.body.style.backgroundColor = rndcol;
 }
 
 );
